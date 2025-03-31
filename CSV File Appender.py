0e7b09aa8c38021df1b8307943fb9af5f1644dd7
jacksonbauersachs <jackrbauersachs@yahoo.com>
2025-03-30 21:47:19 -0400

    Wrote 'CSV File Appender.py'

---
+++ CSV File Appender.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def appendSP_historicalDataDFs(dfMain, dfsToAppendList, outputDF):
+
+    dfMain = pd.read_csv(main_csv_file)
+
+    #Loop through DFs in list and append each
+    for df in dfsToAppendList:
+        dfToAppend = pd.read_csv(csv_file)
+        dfMain = pd.concat([dfMain, dfToAppend], ignore_index=True)
+
+    outputDir = os.path.dirname(outputDF)
+
+    main_df.to_csv(outputDF, index=False)
+
+if __name__ == "__main__":
+
+    dfMain = "S&P 500 Data Sets/S&P 500 Data (7_18_2019 to 3_14_2025).csv"
+
+    fileToAppend1 = "S&P 500 Data Sets/S&P 500 Data (10_01_1999 to 07_17_2019).csv"
+    fileToAppend2 = "S&P 500 Data Sets/S&P 500 Data (12_26_1979 to 9_30_1999).csv"
+
+
+    dfsToAppendList = ["fileToAppend1", "fileToAppend2"]
+
+    outputFolder = "S&P 500 Data Sets"
+    outputDF = 'S&P 500 Data (12_26_1979 to 3_14_2025)'
+    outputDF = os.path.join(output_folder, output_filename)
+
+    appendSP_historicalDataDFs(dfMain, dfsToAppendList, outputDF)
